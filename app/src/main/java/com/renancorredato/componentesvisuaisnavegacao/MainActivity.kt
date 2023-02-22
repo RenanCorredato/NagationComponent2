c31049134fd368a6029741deccc6289109aa0e63
Renan Corredato <renancorredato@hotmail.com>
2023-02-22 18:38:34 -0300

    action bar

--- app/src/main/java/com/renancorredato/componentesvisuaisnavegacao/MainActivity.kt
+++ app/src/main/java/com/renancorredato/componentesvisuaisnavegacao/MainActivity.kt
@@ -2,10 +2,39 @@ package com.renancorredato.componentesvisuaisnavegacao
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.renancorredato.componentesvisuaisnavegacao.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        setupToolbar()
+        setupBottomNavigationView()
     }
+
+    private fun setupBottomNavigationView() {
+        val navHostFragment = getNavHostFragment()
+        binding.bottomNavigationView.setupWithNavController(navHostFragment.navController)
+
+    }
+
+    private fun setupToolbar() {
+        val navHostFragment = getNavHostFragment()
+        val appBarConfiguration = AppBarConfiguration(navHostFragment.navController.graph)
+        binding.toolbar.setupWithNavController(navHostFragment.navController, appBarConfiguration)
+    }
+
+    private fun getNavHostFragment(): NavHostFragment = supportFragmentManager
+        .findFragmentById(binding.fragmentContainerView.id)
+            as NavHostFragment
+
 }
--- app/src/main/java/com/renancorredato/componentesvisuaisnavegacao/MainActivity2.kt
+++ app/src/main/java/com/renancorredato/componentesvisuaisnavegacao/MainActivity2.kt
@@ -2,10 +2,41 @@ package com.renancorredato.componentesvisuaisnavegacao
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.renancorredato.componentesvisuaisnavegacao.databinding.ActivityMain2Binding
 
 class MainActivity2 : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMain2Binding.inflate(layoutInflater)
+    }
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        setContentView(binding.root)
+
+        setupActionBar()
+
     }
+
+    private fun setupActionBar() {
+        val navHostFragment = getNavHostFragment()
+        appBarConfiguration = AppBarConfiguration(navHostFragment.navController.graph)
+        setupActionBarWithNavController(navHostFragment.navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(binding.fragmentContainerView2.id)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    private fun getNavHostFragment(): NavHostFragment = supportFragmentManager
+        .findFragmentById(binding.fragmentContainerView2.id)
+            as NavHostFragment
 }
